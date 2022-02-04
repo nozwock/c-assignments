7fc075d9fb1839df5dda7fd5bcc930058dffffe5
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-05 01:45:03 +0530

    new(lab01): Added lab01 assignments
    All files in a single commit because I made the repo late...my bad

---
+++ lab01/lab01_1_area_of_triangle.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+// use -lm flag for linking math.h
+
+double get_triangle_area(double a, double b, double c);
+
+int main() {
+  double a, b, c;
+  printf("Enter values for sides of triangle: ");
+  scanf("%lf %lf %lf", &a, &b, &c);
+
+  printf("area of triangle = %.3lf\n", get_triangle_area(a, b, c));
+
+  return 0;
+}
+
+double get_triangle_area(double a, double b, double c) {
+  double s = (a + b + c) / 2;
+  return sqrtf(s * (s - a) * (s - b) * (s - c));
+}
---
+++ lab01/lab01_2_net_salary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+double get_net_salary(double bs, double da, double hra, double pf);
+
+int main() {
+  double base_salary;
+  printf("Enter base salary: ");
+  scanf("%lf", &base_salary);
+
+  double da = 0.25f, hra = 0.15f, pf = 0.1f;
+  // pf -> provident fund (base_salary+da+hra)
+  printf("net salary = %.3lf\n", get_net_salary(base_salary, da, hra, pf));
+
+  return 0;
+}
+
+double get_net_salary(double bs, double da, double hra, double pf) {
+  return bs - (pf * (bs + (da * bs) + (hra * bs)));
+}
