878f543e2884e78d8be8a5849a1a0e173c7cc0b3
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-06 01:55:22 +0530

    new(extra): Some extra questions/progs
    Added swap_int prog

---
+++ extra/1_swap_int.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int swap_int(int *a, int *b);
+
+int main(void) {
+    int a = 2, b = 4;
+    printf("before swap: %d %d\n", a, b);
+    swap_int(&a, &b);
+    printf("after swap: %d %d\n", a, b);
+}
+
+int swap_int(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
