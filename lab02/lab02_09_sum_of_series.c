ecc8a89691dbe09bcd1ba616f848d30c47d021f4
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-08 20:46:07 +0530

    new(lab02): Added 9th prog of the assignment

---
+++ lab02/lab02_09_sum_of_series.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// for series
+// 1!+2!+3!+--------------n!
+
+int main(void) {
+    int n;
+    unsigned long long sum = 1, factorial = 1;
+    printf("1!+2!+3!+--------------n!\n");
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+        sum += factorial;
+    }
+
+    printf("sum of series is: %lld\n", sum);
+}
