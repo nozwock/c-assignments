9bd39e284aa92bf21303c3834e2477db614b7684
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-08 20:59:51 +0530

    new(lab02): Added 10th prog of the assignment

---
+++ lab02/lab02_10_sum_of_series.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+// use -lm flag for linking math.h
+
+// for series
+// (1)-(x^1/1!)+(x^2/2!)-............(x^n/n)!
+
+int main(void) {
+    int x, n;
+    unsigned long long sum = 1, factorial = 1;
+    // note: initialized sum with 1 here
+    printf("(1)-(x^1/1!)+(x^2/2!)-............(x^n/n)!\n");
+    printf("Enter value of x, n: ");
+    scanf("%d %d", &x, &n);
+
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+        sum += pow(-x, i) / factorial;
+    }
+
+    printf("sum of series is: %lld\n", sum);
+}
