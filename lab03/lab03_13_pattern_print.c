cd19158197b13e492693dcc2cecd20c22775d4d9
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-09 17:42:58 +0530

    new(lab03): Added 13th prog of the assignment

---
+++ lab03/lab03_13_pattern_print.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+    // 3 2 1
+    // 2 1
+    // 1
+    for (int i = 3; i >= 1; i--) {
+        for (int j = i; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        printf("\b\n");
+    }
+
+    //   *
+    //  * *
+    // * * *
+    // for reference:
+    // http://www.cplusplus.com/reference/cstdio/printf/
+    int size = 3;
+    for (int i = 1; i <= size; i++) {
+        printf("%*s", size - i, "");
+        for (int j = i; j >= 1; j--) {
+            printf("* ");
+        }
+        printf("\b\n");
+    }
+}
