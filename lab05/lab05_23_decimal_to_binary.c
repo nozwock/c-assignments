e3ac4b61fecb310700ff91b2bcfb87add129dbc3
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-15 17:07:38 +0530

    change(lab05): Size returned is now length of string

--- lab05/lab05_23_decimal_to_binary.c
+++ lab05/lab05_23_decimal_to_binary.c
@@ -19,25 +19,24 @@ int main(void) {
 
 StrWithSize decimal_to_binary(int num) {
     StrWithSize bin;
-    bin.size = 1; // for \0 char
-    bin._str = calloc(bin.size, sizeof(char));
+    bin.size = 0;
+    // have the size it returns be 1 less
+    // which is length of string without \0 char
+    bin._str = calloc(bin.size + 1, sizeof(char));
+    bin._str[bin.size] = '\0';
 
-    size_t length = bin.size - 1;
     for (int i = num; i != 0; i /= 2) {
         bin.size++;
-        length = bin.size - 1;
-        bin._str = realloc(bin._str, bin.size * sizeof(char));
-        bin._str[length - 1] = i % 2 + '0';
+        bin._str = realloc(bin._str, bin.size + 1 * sizeof(char));
+        bin._str[bin.size - 1] = i % 2 + '0';
     }
-
-    bin._str[bin.size] = '\0';
 
     // Swap character starting from two corners
     char tmp;
-    for (int i = 0; i < length / 2; i++) {
+    for (int i = 0; i < bin.size / 2; i++) {
         tmp = bin._str[i];
-        bin._str[i] = bin._str[length - i - 1];
-        bin._str[length - i - 1] = tmp;
+        bin._str[i] = bin._str[bin.size - i - 1];
+        bin._str[bin.size - i - 1] = tmp;
     }
 
     return bin;
