75c621c41bb9d77c83bbb413e3ee70bcf9c80aa1
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-15 14:31:43 +0530

    change(lab05): Added a bound check; char only be 0 or 1

--- lab05/lab05_24_binary_to_decimal.c
+++ lab05/lab05_24_binary_to_decimal.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include "../helpers.h"
 
-// DONE: probab just get the size of str via a length() func or something
-// so that there wont be a need to pass many args
 int binary_to_decimal(char[]);
 
 int main(void) {
@@ -16,6 +14,10 @@ int binary_to_decimal(char bin[]) {
     int num = 0;
     int length = get_str_len(bin);
     for (int i = length - 1; i >= 0; i--) {
+        // do a bound check i.e. char should be only 0 or 1
+        if (bin[i] - '0' != 0 && bin[i] - '0' != 1) {
+            return 0;
+        }
         num += (bin[i] - '0') * pow(2, length - 1 - i);
     }
     return num;
