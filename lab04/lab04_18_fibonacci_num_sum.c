e667995943f205901c2fc00e9c0fc4dcf29e33fb
nozwock <57829219+nozwock@users.noreply.github.com>
2022-02-16 16:25:18 +0530

    change(lab04): Removed old code comment

--- lab04/lab04_18_fibonacci_num_sum.c
+++ lab04/lab04_18_fibonacci_num_sum.c
@@ -22,15 +22,5 @@ int64_t get_fibonacci_sum(int n) {
     }
     free(fib._arr);
 
-    // unsigned int fib[n];
-    // unsigned int sum = 1;
-    // fib[0] = 0;
-    // fib[1] = 1;
-
-    // for (int i = 2; i < n; i++) {
-    //     fib[i] = fib[i - 1] + fib[i - 2];
-    //     sum += fib[i];
-    // }
-
     return sum;
 }
