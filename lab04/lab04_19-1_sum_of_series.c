441ff263271a58347e79e6f70a17f017ebed3233
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-02 02:39:51 +0530

    new(lab04): Added a more basic variant of 19th prog

---
+++ lab04/lab04_19-1_sum_of_series.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// for series
+// 1!+2!+-----n!
+
+int main(void)
+{
+    int n;
+    uint64_t sum = 1, factorial = 1;
+    printf("1!+2!+-----n!\n");
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+    n = abs(n);
+
+    for (int i = 2; i <= n; i++) {
+        factorial *= i;
+        sum += factorial;
+    }
+
+    printf("sum of series is: %ld\n", sum);
+}
