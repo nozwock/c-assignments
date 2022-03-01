8fba6b34fcaa9c2301e661758268c80706ed22df
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-02 01:46:18 +0530

    new(lab08): Added incomplete 35th prog

---
+++ lab08/lab08_35_hotels.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// not impl-ing any further due to lack of info
+
+#define NUM_OF_HOTELS 10
+
+struct Hotel {
+    char *name;
+    char *address;
+    int grade;       // ???
+    int room_charge; // ???
+    int num_rooms;
+};
+
+struct Hotel hotels[NUM_OF_HOTELS];
+
+void print_hotel(int grade);
+
+int main(void) {}
+
+void print_hotel(int grade)
+{
+    for (int i = 0; i < NUM_OF_HOTELS; i++) {
+        if (hotels[i].grade == grade) {
+            printf("%s\n", hotels[i].name);
+            return;
+        }
+    }
+}
