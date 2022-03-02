9917c13de7e3e1a16a93dd92ccbf76c732378842
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 05:23:39 +0530

    new(lab09): Added 41st prog

---
+++ lab09/lab09_41_some_pointer_progs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "../lib/helpers.h"
+#include "../lib/sort.h"
+
+// a) To sort the list of numbers through pointer
+// ^ already done before in sort.c (literally just pick any sorting impl)
+
+// b) To reverse the string through pointer
+// ^ already done before in helpers.c (the reverse_string func)
+
+int main(void) {}
