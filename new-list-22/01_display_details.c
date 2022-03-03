f9d2b544900364b74f8e7f1ece83cfc32a8deee7
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 13:22:28 +0530

    new(new-list): Added the 1st prog

---
+++ new-list-22/01_display_details.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // easy to buffer overflow this way but whatever
+    char str_buffer[1000];
+    char *_name = str_buffer;
+    char *_gender;
+    int age;
+    float marks;
+
+    printf("enter your name: ");
+    scanf("%[^\n]s%*c", _name); // name
+
+    printf("enter your age: ");
+    scanf("%d%*c", &age);
+
+    // pointer to a gender string in buffer
+    _gender = _name + strlen(_name) + 2;
+    printf("enter your gender: ");
+    scanf("%[^\n]s%*c", _gender); // gender
+
+    printf("enter your 12th total marks: ");
+    scanf("%f", &marks);
+
+    // displaying
+    printf("%s\n%d\n%s\n%.2f\n", _name, age, _gender, marks);
+}
