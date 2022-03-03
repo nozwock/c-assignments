a7adb46ef61ff78439bd06e8697439b00e01b7a9
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 07:20:30 +0530

    new(lab10): Added 44th prog

---
+++ lab10/lab10_44_factorial_cli.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "../lib/helpers.h"
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        printf("Prints factorial of number\n");
+        printf("Usage: %s [INT]\n", argv[0]);
+        return 1;
+    }
+    uint64_t factorial;
+    if ((factorial = get_factorial(atoi(argv[1]))) < UINT64_MAX / 10) {
+        printf("%lu\n", factorial);
+    } else {
+        printf("MATH Error heHe\n");
+    }
+}
