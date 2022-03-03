d6fb8f34038fc1d48eb1fc3c594cdc09fa363bc5
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 13:45:10 +0530

    new(new-list): Added 4th prog and its test

---
+++ new-list-22/04_roots_of_quadratic.c
@@ -0,0 +1,35 @@
+#include <math.h>
+#include <stdio.h>
+// use -lm flag for linking math.h
+
+int main(void)
+{
+    double a, b, c, discriminant, root1, root2, real_part, img_part;
+    printf("Enter coefficients a, b and c: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    printf("\na=%.2f\nb=%.2f\nc=%.2f\n", a, b, c);
+
+    discriminant = b * b - 4 * a * c;
+
+    // real and different roots
+    if (discriminant > 0) {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("root1 = %.3lf\nroot2 = %.3lf\n", root1, root2);
+    }
+
+    // real and equal roots
+    else if (discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("root1 = root2 = %.3lf\n", root1);
+    }
+
+    // imaginary roots
+    else {
+        real_part = -b / (2 * a);
+        img_part = sqrt(-discriminant) / (2 * a);
+        printf("root1 = %.4lf+%.4lfi\nroot2 = %.4lf-%.4lfi\n", real_part,
+               img_part, real_part, img_part);
+    }
+}
