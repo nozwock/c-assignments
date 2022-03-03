acaa71858bdd02d5322d5b553b0127007607cac8
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 14:59:31 +0530

    new(new-list): Added 15th prog and its test

---
+++ new-list-22/15_matrix_product.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int matrix_sz;
+    printf("enter size of matrix: ");
+    scanf("%d", &matrix_sz);
+
+    int matrix[matrix_sz][matrix_sz];
+    printf("enter elements of matrix:\n");
+    for (int i = 0; i < matrix_sz; i++) {
+        for (int j = 0; j < matrix_sz; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int matrix_square[matrix_sz][matrix_sz];
+    for (int i = 0; i < matrix_sz; i++) {
+        for (int j = 0; j < matrix_sz; j++) {
+            matrix_square[i][j] = 0;
+
+            for (int k = 0; k < matrix_sz; k++) {
+                matrix_square[i][j] += matrix[i][k] * matrix[k][j];
+            }
+        }
+    }
+
+    printf("\nrows = cols = %d\n", matrix_sz);
+
+    printf("original matrix was:\n");
+    for (int i = 0; i < matrix_sz; i++) {
+        for (int j = 0; j < matrix_sz; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("square of matrix is:\n");
+    for (int i = 0; i < matrix_sz; i++) {
+        for (int j = 0; j < matrix_sz; j++) {
+            printf("%d ", matrix_square[i][j]);
+        }
+        printf("\n");
+    }
+}
