e5f419f3345ba083d1b59822a3224a5e2b2248c3
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 13:41:09 +0530

    new(new-list): Added 3rd prog and its test

---
+++ new-list-22/03_new_salary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+double get_net_salary(double bs, double da, double hra, double pf);
+
+int main(void)
+{
+    double base_salary;
+    printf("Enter base salary: ");
+    scanf("%lf", &base_salary);
+
+    double da = 0.25f, hra = 0.15f, pf = 0.1f;
+    // pf -> provident fund (base_salary+da+hra)
+    printf("\nbase salary=%.2f\nda=%.2f\nhra=%.2f\npf=%.2f\n", base_salary, da,
+           hra, pf);
+    printf("net salary = %.3lf\n", get_net_salary(base_salary, da, hra, pf));
+}
+
+double get_net_salary(double bs, double da, double hra, double pf)
+{
+    return bs - (pf * (bs + (da * bs) + (hra * bs)));
+}
