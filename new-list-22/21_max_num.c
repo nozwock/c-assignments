93e8624499f67f27c8a465889b1198f4c2b9ab72
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 15:44:55 +0530

    new(new-list): Added 21st prog

---
+++ new-list-22/21_max_num.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    // set seed with current time
+    srand(time(0));
+
+    int dynamic_arr_sz = 20;
+    int *dynamic_arr = calloc(dynamic_arr_sz, sizeof(*dynamic_arr));
+
+    // populating the array with random numbers and printing them
+    for (int i = 0; i < dynamic_arr_sz; i++) {
+        dynamic_arr[i] = rand() / pow(10, 7);
+        printf("%d ", dynamic_arr[i]);
+    }
+    printf("\n");
+
+    int max_num = dynamic_arr[0];
+    for (int i = 1; i < dynamic_arr_sz; i++) {
+        if (dynamic_arr[i] > max_num) {
+            max_num = dynamic_arr[i];
+        }
+    }
+
+    printf("max number in this array = %d\n", max_num);
+
+    free(dynamic_arr);
+}
