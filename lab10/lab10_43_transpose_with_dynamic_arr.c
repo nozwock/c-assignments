8ddca1f3ecdcae7bf4af07df4488661f4002921a
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 06:54:37 +0530

    change(lab10): Switched to using funcs for allocing 2D arr

--- lab10/lab10_43_transpose_with_dynamic_arr.c
+++ lab10/lab10_43_transpose_with_dynamic_arr.c
@@ -2,29 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// https://stackoverflow.com/questions/28524522/mallocing-a-2d-array-in-c
+
+int **_alloc_int_arr(int nrows, int ncols);
+int free_int_arr(int **ptr_arr, int nrows);
+
 int main(void)
 {
     int matrix_rows, matrix_cols;
     printf("enter size of matrix [row] [columns]: ");
     scanf("%d%d", &matrix_rows, &matrix_cols);
 
-    // allocate memory for 2d array
-    int **matrix = malloc(matrix_rows * sizeof(int *));
-    if (matrix == NULL) {
-        return 1;
-    }
-    // allocate memory for each row
-    for (int i = 0; i < matrix_rows; i++) {
-        matrix[i] = malloc(matrix_cols * sizeof(int));
-        if (matrix[i] == NULL) {
-            // clean memory that was allocated before error
-            while (--i >= 0) {
-                free(matrix[i]);
-            }
-            free(matrix);
-            return 2;
-        }
-    }
+    int **matrix = _alloc_int_arr(matrix_rows, matrix_cols);
 
     printf("enter elements of matrix:\n");
     for (int i = 0; i < matrix_rows; i++) {
@@ -36,23 +25,7 @@ int main(void)
         }
     }
 
-    // allocate memory for 2d array (transpose)
-    int **transpose = malloc(matrix_cols * sizeof(int *));
-    if (transpose == NULL) {
-        return 1;
-    }
-    // allocate memory for each row
-    for (int i = 0; i < matrix_cols; i++) {
-        transpose[i] = malloc(matrix_rows * sizeof(int));
-        if (transpose[i] == NULL) {
-            // clean memory that was allocated before error
-            while (--i >= 0) {
-                free(transpose[i]);
-            }
-            free(transpose);
-            return 2;
-        }
-    }
+    int **transpose = _alloc_int_arr(matrix_cols, matrix_rows);
 
     for (int i = 0; i < matrix_rows; i++) {
         for (int j = 0; j < matrix_cols; j++) {
@@ -68,6 +41,48 @@ int main(void)
         printf("\n");
     }
 
-    free(matrix);
-    free(transpose);
+    free_int_arr(matrix, matrix_rows);
+    free_int_arr(transpose, matrix_cols);
+}
+
+int **_alloc_int_arr(int nrows, int ncols)
+// allocate memory for 2D array and returns pointer if successful or NULL if
+// failed
+{
+    // allocate memory for nrows
+    int **ptr_arr = malloc(nrows * sizeof(int *));
+    if (ptr_arr == NULL) {
+        return NULL;
+    }
+    // allocate memory for each row
+    for (int r = 0; r < nrows; r++) {
+        ptr_arr[r] = malloc(ncols * sizeof(int));
+        if (ptr_arr[r] == NULL) {
+            // clean memory that was allocated before error
+            while (--r >= 0) {
+                free(ptr_arr[r]);
+            }
+            free(ptr_arr);
+            return NULL;
+        }
+    }
+    return ptr_arr;
+}
+
+int free_int_arr(int **ptr_arr, int nrows)
+// frees memory allocated for 2D array and returns 1 if successful or 0 if
+// failed
+{
+    if (ptr_arr == NULL || nrows < 1) {
+        return 0;
+    }
+    // free memory allocated for each row
+    for (int r = 0; r < nrows; r++) {
+        if (ptr_arr[r] != NULL) {
+            free(ptr_arr[r]);
+        }
+    }
+    // free memory allocated for rows pointers
+    free(ptr_arr);
+    return 1;
 }
