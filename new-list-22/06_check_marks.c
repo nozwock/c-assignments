52332874fd2a55a5e736e964418db9a7e4e1129e
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 13:51:37 +0530

    new(new-list): Added 6th prog

---
+++ new-list-22/06_check_marks.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float phy_marks, che_marks, math_marks;
+    printf("Enter marks for physics, chemistry, maths: ");
+    scanf("%f %f %f", &phy_marks, &che_marks, &math_marks);
+
+    printf("\nphy=%.2f\nche=%.2f\nmath=%.2f\n", phy_marks, che_marks,
+           math_marks);
+
+    if (phy_marks >= 40 && che_marks >= 50 && math_marks >= 60 &&
+        (phy_marks + math_marks >= 150 ||
+         phy_marks + che_marks + math_marks >= 200)) {
+        printf("eligible for course\n");
+    } else {
+        printf("not eligible for course\n");
+    }
+}
