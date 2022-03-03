52bcccf5a92106963a4b65bfa8b04ddd4d32fde2
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-03 13:36:27 +0530

    new(new-list): Added 2nd prog and its test

---
+++ new-list-22/02_area_of_triangle.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdio.h>
+// use -lm flag for linking math.h
+
+double get_triangle_area(double a, double b, double c);
+
+int main(void)
+{
+    double a, b, c;
+    printf("Enter values for sides of triangle: ");
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    printf("\n");
+    printf("entered sides are: %.2lf %.2lf %.2lf\n", a, b, c);
+    printf("area of triangle = %.2lf\n", get_triangle_area(a, b, c));
+}
+
+double get_triangle_area(double a, double b, double c)
+{
+    double s = (a + b + c) / 2;
+    return sqrtf(s * (s - a) * (s - b) * (s - c));
+}
