f7802a98a7c5b989a5092af6a44149f56876c147
nozwock <57829219+nozwock@users.noreply.github.com>
2022-03-07 08:53:56 +0530

    change(new-list): Corrected the 15th prog

--- new-list-22/15_matrix_product.c
+++ new-list-22/15_matrix_product.c
@@ -2,43 +2,41 @@
 
 int main(void)
 {
-    int matrix_sz;
-    printf("enter size of matrix: ");
-    scanf("%d", &matrix_sz);
+    int order;
+    printf("enter order of matrix: ");
+    scanf("%d", &order);
 
-    int matrix[matrix_sz][matrix_sz];
-    printf("enter elements of matrix:\n");
-    for (int i = 0; i < matrix_sz; i++) {
-        for (int j = 0; j < matrix_sz; j++) {
-            scanf("%d", &matrix[i][j]);
+    int matrix1[order][order];
+    printf("enter elements of matrix1:\n");
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            scanf("%d", &matrix1[i][j]);
         }
     }
 
-    int matrix_square[matrix_sz][matrix_sz];
-    for (int i = 0; i < matrix_sz; i++) {
-        for (int j = 0; j < matrix_sz; j++) {
-            matrix_square[i][j] = 0;
+    int matrix2[order][order];
+    printf("enter elements of matrix2:\n");
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
 
-            for (int k = 0; k < matrix_sz; k++) {
-                matrix_square[i][j] += matrix[i][k] * matrix[k][j];
+    int product[order][order];
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            product[i][j] = 0;
+
+            for (int k = 0; k < order; k++) {
+                product[i][j] += matrix1[i][k] * matrix2[k][j];
             }
         }
     }
 
-    printf("\nrows = cols = %d\n", matrix_sz);
-
-    printf("original matrix was:\n");
-    for (int i = 0; i < matrix_sz; i++) {
-        for (int j = 0; j < matrix_sz; j++) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("square of matrix is:\n");
-    for (int i = 0; i < matrix_sz; i++) {
-        for (int j = 0; j < matrix_sz; j++) {
-            printf("%d ", matrix_square[i][j]);
+    printf("product of matrix is:\n");
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            printf("%d ", product[i][j]);
         }
         printf("\n");
     }
